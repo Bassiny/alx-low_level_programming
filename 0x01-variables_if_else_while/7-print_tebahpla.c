28cdb2fe3570e3f152c7fe310205aee02768e644
Bassiny <youssef.bassini@gmail.com>
2023-06-19 15:44:05 -0700

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Rreverse alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter = 'z';
+
+	while (letter >= 'a')
+	{
+		putchar(letter);
+		letter--;
+	}
+	putchar('\n');
+	return (0);
+}
