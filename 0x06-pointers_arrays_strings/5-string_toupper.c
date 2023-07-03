ff2cbe69d432eae83a2bd4d794841bbbcbf178ed
Bassiny <youssef.bassini@gmail.com>
2023-07-03 16:42:31 -0700

    5. Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * string_toupper -  changes all lowercase letters of a string to uppercase
+ * @n: the string lowercase
+ * Return: string uppercase
+ */
+char *string_toupper(char *n)
+{
+	int i = 0;
+
+	while (n[i] != '\0')
+	{
+		if (n[i]<= 122 && n[i] >= 97)
+			n[i] = n[i] - 32;
+		i++;
+	}
+	return (n);
+}
